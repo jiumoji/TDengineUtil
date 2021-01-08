68ee727413596077abf1cc5e1bd88d5b016d4bcb
tzp <admin@example.com>
2021-01-08 15:09:10 +0800

    test

--- src/cn/netuo/util/Main.java
+++ src/cn/netuo/util/Main.java
@@ -14,6 +14,7 @@ public class Main
         util.insertWithStable("a1_001","netuo_device",new cn.netuo.util.IotReceiptUploadDataEntity(new Date(),null,"33.3",true,"12","1234",12,"22.3","30","14",new Date()),"样子将中路","ce","1");
 
 
+
         List<IotReceiptUploadDataEntity> list = util.getList("select * from  v1_001",IotReceiptUploadDataEntity.class);
         System.out.println(list);
     }
